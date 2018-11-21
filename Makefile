87fa66d18ac14981ed603b494d5a79be3cab64c0
Peter Murphy <peter.francis.murphy@gmail.com>
2018-11-21 11:20:04 -0500

    Makefile for topojson

--- Makefile
+++ Makefile
@@ -1,10 +1,34 @@
 .PHONY: all clean test
 
 # Download geojson
-data/util.geojson:
+json/raw.json:
 	@mkdir -p $(dir $@)
 	@curl \
 		-sS \
 		"http://maps-massgis.opendata.arcgis.com/datasets/1710ebf6cf614b5fa97c0a269cece375_0.geojson"
 		-o $@.download
 	@mv $@.download $@
+
+
+json/ma-projected.json: json/raw.json
+	@geoproject "d3.geoConicConformal().parallels([41 + 43 / 60, 42 + 41 / 60]).rotate([71 + 30 / 60, 0]).fitSize([960, 960], d)" \
+		$< \
+		> $@
+
+topojson/ma-topo.json: json/ma-projected.json
+	@mkdir -p $(dir $@)
+	@geo2topo \
+		$< \
+		> $@
+
+topojson/ma-simple-topo.json: topojson/ma-topo.json
+	@toposimplify -p 1 -f \
+		< $< \
+		> $@
+
+topojson/ma-quantized-topo.json: topojson/ma-simple-topo.json
+	@topoquantize 1e5 \
+		< $< \
+		> $@
+
+all: topojson/ma-quantized-topo.json
--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-import { compose, filter, head } from "ramda"
+import {
+  compose,
+  equals,
+  filter,
+  head,
+  groupBy,
+  groupWith,
+  lensPath,
+  map,
+  prop,
+  view
+} from "ramda"
 import { readFileSync } from "fs"
+import { geoConicConformal } from "d3"
+
+const projection = d =>
+  geoConicConformal()
+    .parallels([41 + 43 / 60, 42 + 41 / 60])
+    .rotate([71 + 30 / 60, 0])
+    .fitSize([960, 960], d)
 
 const data = compose(
   JSON.parse,
   readFileSync
 )("./data/util.geojson")
+
+data.features[1].properties.ELEC_LABEL //?
+
+data.features.map(
+  ({ properties: { ELEC_LABEL } }) => ELEC_LABEL
+)
+//?
+
+compose(
+  map(prop("ELEC_LABEL")),
+  map(view(lensPath(["properties"]))),
+  prop("features")
+)(data) //?
+
+groupBy(view(lensPath(["properties", "ELEC_LABEL"])))(
+  data.features
+) //?
